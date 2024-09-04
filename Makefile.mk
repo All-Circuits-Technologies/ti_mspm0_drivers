76816bae4f1c54fab47ea9f5f59f802cd3b5ab2e
Pierre-Noel Bouteville <pierre-noel.bouteville@allcircuits.com>
2024-09-04 11:29:01 +0200

    license + gitignore

--- Makefile.mk
+++ Makefile.mk
@@ -1,11 +1,8 @@
 # SPDX-FileCopyrightText: 2022 Pierre-Noël Bouteville <pierre-noel.bouteville@allcircuits.com>
+# SPDX-FileCopyrightText: 2024 Pierre-Noel Bouteville <pnb990@gmail.com>
 #
 # SPDX-License-Identifier: LicenseRef-ALLCircuits-ACT-1.1
+# SPDX-License-Identifier: BSD-3-Clause
 #
 
-CPU_DRIVERS_DIR:=$(dir $(lastword $(MAKEFILE_LIST)))
-
-INCLUDEPATHS+=$(CPU_DRIVERS_DIR)
-INCLUDEPATHS+=$(CPU_DRIVERS_DIR)/ti/CMSIS/Core/Include
-INCLUDEPATHS+=$(CPU_DRIVERS_DIR)/ti/CMSIS/DSP/Include
-
+LIB_TI_MSPM0_DRIVERS_DIR:=$(dir $(lastword $(MAKEFILE_LIST)))
